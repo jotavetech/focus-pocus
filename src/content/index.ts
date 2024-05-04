e26c94e93b4cf2acb83be677b044c487ae1c00c9
jotavetech <ojoaogugel@gmail.com>
2024-05-03 23:34:38 -0300

    feat: add content page

---
+++ src/content/index.ts
@@ -0,0 +1,55 @@
+let urlLinks: string[] = ["instagram.com", "twitter.com"];
+
+let tabStates: Map<string, boolean> = new Map();
+
+// chrome.storage.local.get(["urlList"], (res) => {
+//   urlLinks = res.urlList || [];
+// });
+
+chrome.storage.local.get(["isRunning"], (res) => {
+  if (res.isRunning) {
+    addFocusPage();
+  }
+});
+
+chrome.storage.onChanged.addListener((changes) => {
+  if (changes.isRunning && changes.isRunning.newValue) {
+    addFocusPage();
+  }
+
+  if (changes.isRunning && !changes.isRunning.newValue) {
+    const focusPage = document.querySelector("#focus-page");
+    if (focusPage) {
+      focusPage.remove();
+      const tabId = getCurrentTabId()!;
+      tabStates.delete(tabId);
+    }
+  }
+});
+
+function addFocusPage() {
+  const currentTabId = getCurrentTabId()!;
+  if (!tabStates.get(currentTabId)) {
+    for (let url of urlLinks) {
+      if (window.location.href.includes(url)) {
+        let focusPage = document.createElement("div");
+        focusPage.id = "focus-page";
+        focusPage.innerHTML = `
+          <div id="focus-page-content">
+            <h1>Focus Mode</h1>
+            <p>Time to focus on your work.</p>
+            <p>If you give up, your streak will be reset.</p>
+          </div>
+          `;
+        document.querySelector("body")!.appendChild(focusPage);
+
+        tabStates.set(currentTabId, true);
+        return;
+      }
+    }
+  }
+}
+
+function getCurrentTabId() {
+  return new URLSearchParams(window.location.search).get("tabId");
+}
