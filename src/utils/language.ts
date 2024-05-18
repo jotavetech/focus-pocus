b37f03962823e9061b61522496ee155846f7db1d
jotavetech <ojoaogugel@gmail.com>
2024-05-18 12:07:18 -0300

    build: update version

---
+++ src/utils/language.ts
@@ -0,0 +1,61 @@
+const enLanguage = {
+  popup: {
+    startButton: {
+      start: "START FOCUSING",
+      giveUp: "GIVE UP!",
+    },
+    timer: {
+      timeLabel: "Custom Time",
+    },
+    display: {
+      hour: "HOUR",
+      hours: "HOURS",
+    },
+  },
+  options: {
+    switch: {
+      language: "Change language",
+      giveupSound: "Give up sound",
+      finishSound: "Finish sound",
+      startSound: "Start/Stop button sound",
+    },
+    urlList: {
+      subtitle: "All the websites bellow will be blocked in focus mode.",
+      input: "Enter a url to block",
+      removeAll: "Remove All",
+    },
+    donate: "Donate ❤️",
+  },
+};
+
+const ptLanguage = {
+  popup: {
+    startButton: {
+      start: "COMEÇAR A FOCAR",
+      giveUp: "DESISTIR!",
+    },
+    timer: {
+      timeLabel: "Tempo Personalizado",
+    },
+    display: {
+      hour: "HORA",
+      hours: "HORAS",
+    },
+  },
+  options: {
+    switch: {
+      language: "Mudar idioma",
+      giveupSound: "Som de desistência",
+      finishSound: "Som de finalização",
+      startSound: "Som do botão de início/parada",
+    },
+    urlList: {
+      subtitle: "Todos os sites abaixo serão bloqueados no modo de foco.",
+      input: "Insira um url para bloquear",
+      removeAll: "Remover Todos",
+    },
+    donate: "Apoiar ❤️",
+  },
+};
+
+export { enLanguage, ptLanguage };
