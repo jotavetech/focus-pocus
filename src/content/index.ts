cd3ac04a541eb0291efac5c116d8f65ff9cf0aa2
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2024-05-24 18:12:53 -0300

    feat: watch new URLs and simplify code

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,77 +1,74 @@
 import browser from "webextension-polyfill";
 
 let urlLinks: string[] = [];
-
-let tabStates: Map<string, boolean> = new Map();
+let isRunning = false
 
 browser.storage.local.get(["blockList", "isRunning"]).then((res) => {
   urlLinks = res.blockList || [];
+  isRunning = res.isRunning ?? false
 
-  if (res.isRunning) {
-    addFocusPage();
-  }
+  main()
 });
 
 browser.storage.onChanged.addListener((changes) => {
   if (changes.blockList) {
     urlLinks = changes.blockList.newValue;
+    main()
   }
 
-  if (changes.isRunning && changes.isRunning.newValue) {
-    addFocusPage();
-  }
-
-  if (changes.isRunning && !changes.isRunning.newValue) {
-    const focusPage = document.querySelector("#focus-page");
-    if (focusPage) {
-      focusPage.remove();
-      const tabId = getCurrentTabId()!;
-      tabStates.delete(tabId);
-    }
+  if (changes.isRunning) {
+    isRunning = changes.isRunning.newValue
+    main();
   }
 });
 
-browser.runtime.onMessage.addListener((message) => {
-  if (message.type === "TIMER_FINISHED") {
-    const focusPage = document.querySelector("#focus-page");
-    if (focusPage) {
-      focusPage.remove();
-      const tabId = getCurrentTabId()!;
-      tabStates.delete(tabId);
-    }
-  }
-});
-
-function addFocusPage() {
-  const currentTabId = getCurrentTabId()!;
-  if (!tabStates.get(currentTabId)) {
-    for (let url of urlLinks) {
-      if (window.location.href.includes(url)) {
-        let focusPage = document.createElement("div");
-        focusPage.id = "focus-page";
-        focusPage.innerHTML = `
-          <div id="focus-page-content">
-            <h1>Focus Mode</h1>
-            <p>Time to focus on your work.</p>
-            <p>If you give up, your streak will be reset.</p>
-          </div>
-          `;
-        document.querySelector("body")!.appendChild(focusPage);
-
-        document.querySelectorAll("video").forEach((video) => {
-          video.pause();
-        });
-        document.querySelectorAll("audio").forEach((audio) => {
-          audio.pause();
-        });
-
-        tabStates.set(currentTabId, true);
-        return;
-      }
-    }
-  }
+function getFocusPage() {
+  return document.querySelector("#focus-page")
 }
 
-function getCurrentTabId() {
-  return new URLSearchParams(window.location.search).get("tabId");
+function hasFocusPage() {
+  return !!getFocusPage()
+}
+
+function removeFocusPage() {
+  getFocusPage()?.remove()
+}
+
+function addFocusPage() {
+  const focusPage = document.createElement("div");
+  focusPage.id = "focus-page";
+  focusPage.innerHTML = `
+    <div id="focus-page-content">
+      <h1>Focus Mode</h1>
+      <p>Time to focus on your work.</p>
+      <p>If you give up, your streak will be reset.</p>
+    </div>
+    `;
+
+  if(hasFocusPage()){
+    getFocusPage()?.replaceWith(focusPage)
+  }else {
+    document.body.appendChild(focusPage);
+  }
+
+  document.querySelectorAll("video").forEach((video) => {
+    video.pause();
+  });
+
+  document.querySelectorAll("audio").forEach((audio) => {
+    audio.pause();
+  });
+}
+
+
+function main() {
+  const shouldBlockPage = isRunning && urlLinks.some(url => window.location.href.includes(url))
+
+  if(shouldBlockPage){
+    addFocusPage()
+  }
+
+  if(!shouldBlockPage && hasFocusPage()){
+    removeFocusPage()
+  }
 }
