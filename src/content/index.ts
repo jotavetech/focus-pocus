59e97ba7f262d6af2efc47572fb94e4b82fb7f93
jotavetech <ojoaogugel@gmail.com>
2024-06-02 15:26:39 -0300

    ...

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,116 +1,90 @@
-import browser, { urlbar } from "webextension-polyfill";
+import browser from "webextension-polyfill";
 
 let blocklist: string[] = [];
 let allowlist: string[] = [];
 
 let allowlistMode = false;
 
-let tabStates: Map<string, boolean> = new Map();
+function initialize() {
+  browser.storage.local
+    .get(["blocklist", "allowlist", "isRunning", "options"])
+    .then((res) => {
+      blocklist = res.blocklist || [];
+      allowlist = res.allowlist || [];
 
-console.log("aqui");
+      if (res.options && res.options["allowlist-mode"]) {
+        allowlistMode = true;
+      }
 
-browser.storage.local
-  .get(["blocklist", "allowlist", "isRunning", "options"])
-  .then((res) => {
-    blocklist = res.blocklist;
-    allowlist = res.allowlist;
-
-    if (res.options["allowlist-mode"]) {
-      console.log("ta tendo ein");
-      allowlistMode = true;
-    }
-
-    if (res.isRunning) {
-      checkFocusPage();
-    }
-  });
+      if (res.isRunning) {
+        checkFocusPage();
+      }
+    });
+}
 
 browser.storage.onChanged.addListener((changes) => {
-  if (changes.blocklist) {
-    blocklist = changes.blocklist.newValue;
-  }
+  if (changes.blocklist) blocklist = changes.blocklist.newValue || [];
+  if (changes.allowlist) allowlist = changes.allowlist.newValue || [];
 
-  if (changes.allowlist) {
-    allowlist = changes.allowlist.newValue;
-  }
-
-  if (changes.isRunning && changes.isRunning.newValue) {
-    checkFocusPage();
-  }
-
-  if (changes.options && changes.options.newValue["allowlist-mode"]) {
-    allowlistMode = true;
-  } else {
-    allowlistMode = false;
-  }
-
-  if (changes.isRunning && !changes.isRunning.newValue) {
-    const focusPage = document.querySelector("#focus-page");
-    if (focusPage) {
-      focusPage.remove();
-      const tabId = getCurrentTabId()!;
-      tabStates.delete(tabId);
+  if (changes.options) {
+    if (
+      changes.options.newValue &&
+      changes.options.newValue["allowlist-mode"]
+    ) {
+      allowlistMode = true;
+    } else {
+      allowlistMode = false;
     }
   }
-});
 
-browser.runtime.onMessage.addListener((message) => {
-  if (message.type === "TIMER_FINISHED") {
-    const focusPage = document.querySelector("#focus-page");
-    if (focusPage) {
-      focusPage.remove();
-      const tabId = getCurrentTabId()!;
-      tabStates.delete(tabId);
+  if (changes.isRunning) {
+    if (changes.isRunning.newValue) {
+      checkFocusPage();
+    } else {
+      removeFocusPage();
     }
   }
 });
 
 function checkFocusPage() {
-  const currentTabId = getCurrentTabId()!;
-  if (!tabStates.get(currentTabId)) {
-    if (allowlistMode) {
-      for (let url of allowlist) {
-        if (window.location.href.includes(url)) {
-          return;
-        }
+  removeFocusPage();
+  checkMode();
+}
 
-        addFocusPage(currentTabId);
-      }
+function checkMode() {
+  if (allowlistMode) {
+    for (let url of allowlist) {
+      if (window.location.href.includes(url)) return;
     }
-
-    if (!allowlistMode) {
-      for (let url of blocklist) {
-        if (window.location.href.includes(url)) {
-          addFocusPage(currentTabId);
-        }
+    addFocusPage();
+  } else {
+    for (let url of blocklist) {
+      if (window.location.href.includes(url)) {
+        addFocusPage();
+        return;
       }
     }
   }
 }
 
-function addFocusPage(currentTabId: string) {
-  let focusPage = document.createElement("div");
+function addFocusPage() {
+  const body = document.querySelector("body");
+  const focusPage = document.createElement("div");
   focusPage.id = "focus-page";
   focusPage.innerHTML = `
-          <div id="focus-page-content">
-            <h1>Focus Mode</h1>
-            <p>Time to focus on your work.</p>
-            <p>If you give up, your streak will be reset.</p>
-          </div>
-          `;
-  document.querySelector("body")!.appendChild(focusPage);
+    <div id="focus-page-content">
+      <h1>Focus Mode</h1>
+      <p>Time to focus on your work.</p>
+      <p>If you give up, your streak will be reset.</p>
+    </div>
+  `;
 
-  document.querySelectorAll("video").forEach((video) => {
-    video.pause();
-  });
-  document.querySelectorAll("audio").forEach((audio) => {
-    audio.pause();
-  });
-
-  tabStates.set(currentTabId, true);
-  return;
+  if (body) body.appendChild(focusPage);
 }
 
-function getCurrentTabId() {
-  return new URLSearchParams(window.location.search).get("tabId");
+function removeFocusPage() {
+  const focusPage = document.querySelector("#focus-page");
+  if (focusPage) focusPage.remove();
 }
+
+initialize();
